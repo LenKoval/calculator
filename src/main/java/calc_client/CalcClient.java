e27a423b5f4ecf61dd09bbb3cfbb166c6dd5cbfa
{ElenaKovaleva} <{lenshemeta@gmail.com}>
2023-09-28 14:24:43 +0300

    lesson20hm13 исправлен CalcClient

--- src/main/java/calc_client/CalcClient.java
+++ src/main/java/calc_client/CalcClient.java
@@ -13,8 +13,9 @@ public class CalcClient {
         try (Socket socket = new Socket("localhost", 8088);
              DataInputStream in = new DataInputStream(socket.getInputStream());
              DataOutputStream out = new DataOutputStream(socket.getOutputStream())) {
+            System.out.println("Это калькулятор\nДоступные мат. операции : '+', '-', '*', '/' \nВЫХОД - для остановки приложения");
             while (true) {
-                System.out.println("\nЭто Калькулятор.\nДоступные мат. операции : '+', '-', '*', '/' \nВЫХОД - для остановки приложения\nВведите уравнение:");
+                System.out.println("Введите уравнение:");
                 String mes = scanner.nextLine();
                 if (mes.equals("ВЫХОД")) break;
                 out.writeUTF(mes);
